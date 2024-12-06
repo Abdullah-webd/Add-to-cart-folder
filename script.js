05ee0fb1b211fe1947fde7a50f65003784750854
Abdullah-webd <ajalabdllh@gmail.com>
2024-12-06 18:56:34 +0100

    save changes

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ const addbutton = document.querySelector('.add');
 const subsbutton = document.querySelector('.subs');
 const addcartbutton = document.querySelector('.addcart')
 const doneshop = document.querySelector('.donebutton')
+const bluecloth = document.querySelector('#picone')
+const redcloth = document.querySelector('#pictwo')
+const whitecloth = document.querySelector('#picthree')
+const organiccontain = document.querySelector('.orgpic')
 let counter = 0; // Declare counter outside the event listener
 
 
+
+bluecloth.addEventListener('click', function() {
+    organiccontain.innerHTML = `<img src="clothimage3 (2).webp" alt="" class="difdesign" id="picone" style=" width: 500px;">`
+});
+
+redcloth.addEventListener('click', function() {
+    organiccontain.innerHTML = `<img src="clothimage3.webp" alt="" class="difdesign" id="pictwo" style=" width: 500px;">`
+});
+
+whitecloth.addEventListener('click', function() {
+    organiccontain.innerHTML = `<img src="clothimage.webp" alt="" class="difdesign" id="picthree" style=" width: 500px;">`
+});
 
 doneshop.addEventListener('click', function() {
     alert(`You have sucessfull purchased the Official Mr martins T-shirt Quantity: ${counter}`)
